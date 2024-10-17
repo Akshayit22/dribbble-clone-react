757cbb907588493fdfc948da9935d580d55c9c49
Akshay Telang <akshayit22@gmail.com>
2024-10-17 14:39:25 +0530

    Trying tailwindcss best practices

--- dribbble-clone/src/pages/common/Try.tsx
+++ dribbble-clone/src/pages/common/Try.tsx
@@ -2,11 +2,23 @@
 
 // import { Loader } from "../../common/Home/Loader"
 
+const myStyles = {
+    container: 'h-10 bg-black text-white',
+    childs: "h-10 bg-white text-black",
+    hover:"hover:text-pink-300"
+};
+
 export const Try = () => {
 
 
     return (
         <div>
+            <div className={`${myStyles.container}  ${myStyles.hover}`}>
+                container
+                <div className={`${myStyles.childs} ${myStyles.hover}`}>
+                    childs
+                </div>
+            </div>
             {/* <DropdownMenu/> */}
             
             {/* <Loader/> */}
